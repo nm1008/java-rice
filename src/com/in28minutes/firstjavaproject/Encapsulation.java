fb959458253c50119e4bb0b745283a04e01e6924
nm1008 <mallari08nikko@yahoo.com>
2024-03-29 09:09:45 +0800

    encapsulate

---
+++ src/com/in28minutes/firstjavaproject/Encapsulation.java
@@ -0,0 +1,46 @@
+package com.in28minutes.firstjavaproject;
+
+public class Encapsulation {
+
+	private int speed;
+	private String name;
+
+	// constructor
+	public Encapsulation(String name) {
+		this.name = name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	void start() {
+		if (this.speed < 0) {
+			System.out.println("Starting, Invalid bike speed");
+		}
+
+		System.out.println(this.name + " Bike start");
+		System.out.println(this.speed);
+
+	};
+
+	void setSpeed(int speed) {
+		if (speed >= 0) {
+			this.speed = speed;
+		}
+	}
+
+	public void increaseSpeed(int howMuch) {
+//		this.speed = this.speed + howMuch;
+		setSpeed(this.speed + howMuch);
+	}
+
+	public void decreaseSpeed(int howMuch) {
+		setSpeed(this.speed - howMuch);
+	}
+
+}
--- src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
+++ src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
@@ -4,10 +4,46 @@ public class MultiplicationTableRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//
+//		SumOfSquares square = new SumOfSquares();
+//
+//		square.calculateSumOfSquares(2);
 
-		Binumber numbers = new Binumber(2,3);
-		
-		System.out.println(numbers.add());
+//		Binumber numbers = new Binumber(2, 3);
+//
+//		System.out.println(numbers.add());
+//		System.out.println(numbers.multiply());
+//
+//		numbers.doubleValue();
+//
+//		System.out.println(numbers.getNumber1());
+//		System.out.println(numbers.getNumber2());
+
+//		Encapsulation ducati = new Encapsulation();
+//		Encapsulation honda = new Encapsulation();
+
+//		honda.speed(125);
+//		honda.start();
+
+//		ducati.start();
+//		ducati.setSpeed(250);
+//		ducati.start();
+
+		Encapsulation ducati = new Encapsulation("Ducati");
+		Encapsulation honda = new Encapsulation("Honda");
+
+//		ducati.start();
+//		System.out.println(ducati.getName());
+
+//		ducati.start();
+//		System.out.println(ducati.getName());
+//		ducati.setName("New Ducati");
+//		System.out.println(ducati.getName());
+
+//		ducati.setSpeed(1);
+//		ducati.increaseSpeed(149);
+//		ducati.start();
+		ducati.decreaseSpeed(200);
+		ducati.start();
 	}
-
 }
